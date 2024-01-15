83ae2cbdcd3f182d65efa63db843c2d41db4d566
mohsensami <mohsensami88@gmail.com>
2024-01-15 20:37:41 +0330

    Register by nextAuth

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import Link from 'next/link';
+import { register } from '@/lib/action';
 
-export default function Register() {
-    return <div>Register</div>;
-}
+const Register = () => {
+    return (
+        <div>
+            <div>
+                <form action={register}>
+                    <input type="text" placeholder="username" name="username" />
+                    <input type="email" placeholder="email" name="email" />
+                    <input type="password" placeholder="password" name="password" />
+                    <input type="password" placeholder="password again" name="passwordRepeat" />
+                    <button>Register</button>
+                    {/* <Link href="/login">
+                        Have an account? <b>Login</b>
+                    </Link> */}
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
--- src/lib/action.js
+++ src/lib/action.js
@@ -56,3 +56,38 @@ export const handleLogout = async () => {
     'use server';
     await signOut();
 };
+
+export const register = async (formData) => {
+    const { username, email, password, passwordRepeat } = Object.fromEntries(formData);
+
+    if (password !== passwordRepeat) {
+        return { error: 'Passwords do not match' };
+    }
+
+    try {
+        connectToDb();
+
+        const user = await User.findOne({ username });
+
+        if (user) {
+            return { error: 'Username already exists' };
+        }
+
+        // const salt = await bcrypt.genSalt(10);
+        // const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            username,
+            email,
+            password,
+        });
+
+        await newUser.save();
+        console.log('saved to db');
+
+        return { success: true };
+    } catch (err) {
+        console.log(err);
+        return { error: 'Something went wrong!' };
+    }
+};
