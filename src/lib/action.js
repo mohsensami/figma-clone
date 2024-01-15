c5e5bb929b5461d6061c1bbb4e30a0e33d0fcc26
mohsensami <mohsensami88@gmail.com>
2024-01-15 20:46:12 +0330

    hash password by bcryptjs

--- src/lib/action.js
+++ src/lib/action.js
@@ -4,6 +4,7 @@ import { revalidatePath } from 'next/cache';
 import { Post, User } from './models';
 import { connectToDb } from './utils';
 import { signIn, signOut } from './auth';
+import bcrypt from 'bcryptjs';
 
 export const addPost = async (formData) => {
     // const title = formData.get("title");
@@ -73,13 +74,13 @@ export const register = async (formData) => {
             return { error: 'Username already exists' };
         }
 
-        // const salt = await bcrypt.genSalt(10);
-        // const hashedPassword = await bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = new User({
             username,
             email,
-            password,
+            password: hashedPassword,
         });
 
         await newUser.save();
