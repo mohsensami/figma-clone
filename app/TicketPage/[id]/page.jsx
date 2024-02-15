67faa4cf57b04f9c6f49d58ec469bea687866ea9
mohsensami <mohsensami88@gmail.com>
2024-02-15 16:52:36 +0330

    initial ticket app

---
+++ app/TicketPage/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TicketSinglePage = ({ params }) => {
+    return <div>TicketSinglePage {params.id}</div>;
+};
+
+export default TicketSinglePage;
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <h1 className="text-5xl text-white">Liveblocks Figma Clone</h1>;
-}
